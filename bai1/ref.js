82424684c7032550bed153edcc8c4d93f6a12f8f
pearlde-bink <tuanbinhnguyenngoc@gmail.com>
2024-01-12 10:04:02 +0700

    some first assignments

---
+++ bai1/ref.js
@@ -0,0 +1,11 @@
+// const submitBut = document.querySelector(".formBut");
+
+// submitBut.addEventListener("click", updateBut);
+
+// function updateBut() {
+//     if (submitBut.value === "Submit") {
+//         submitBut.value = "hehe";
+//     } else {
+//         submitBut.value = "Submit";
+//     }
+// }
